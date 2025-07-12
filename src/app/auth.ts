4c3d9b525aeb74eb7fb6965bf43e28ad1941854e
swappy <59965507+rycerzes@users.noreply.github.com>
2025-07-12 17:13:40 +0530

    fix: update google scopes for gmail

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -14,6 +14,19 @@ export const {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: [
+            "openid",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/gmail.send",
+            "https://mail.google.com/",
+            "https://www.googleapis.com/auth/gmail.modify",
+            "https://www.googleapis.com/auth/gmail.compose"
+          ].join(' ')
+        }
+      }
     }),
   ],
   callbacks: {},
